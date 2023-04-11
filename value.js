a45551964ceda2732b9b10b3efe02eb98d81f9c8
郭晋良 <eric@kiaofarming.com>
2023-04-11 15:48:09 +0800

    Test fs sync.

--- value.js
+++
@@ -1,1 +0,0 @@
-var data = {"AP":"webduino.io", "IP":"192.168.10.127", "MAC":"90:38:0C:6A:A8:BC", "Ver":"0.0.1 (Feb  1 2023:11:17:22)", "ssid":"Eripmav", "passwd":"0939719621", "devId":"wdEaV", "devSSID":"wf8001", "devPasswd":"12345678", "openAp":"Yes", "broker":"mqtt.kiaofarming.com", "port":1883}
